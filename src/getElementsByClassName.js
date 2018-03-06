5e3275e35038298b1f4705eceddba38c3f7dfa6f
ahmadaladdasi <ahmadaladdasi88@gmail.com>
2018-03-05 17:52:19 -0800

    updated all files to pass minimum requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
-  // your code here
+  var resultArr = [];
+  var node = node || document.body;
+  
+  if(node.classList !== undefined){  
+    if(node.classList.contains(className)) {
+     resultArr.push(node); 
+    }
+  }
+ if(node.childNodes.length !== 0) {
+  node.childNodes.forEach(childNode => {
+  //console.log(childNode);
+    resultArr = resultArr.concat(getElementsByClassName(className, childNode));
+    //console.log(resultArr);
+  });
+}
+  console.log(resultArr)
+  return resultArr; 
 };
